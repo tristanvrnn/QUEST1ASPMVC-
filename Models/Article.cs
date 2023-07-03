7a007c524bb682170db116218e50c8502467ba32
tristanvrnn <134289144+tristanvrnn@users.noreply.github.com>
2023-07-03 17:49:32 +0200

    Add files via upload

---
+++ Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace QUEST1ASPMVC.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QUEST1ASPMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
